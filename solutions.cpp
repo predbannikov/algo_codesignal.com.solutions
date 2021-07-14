d9ea4dc32ef51e179861d17e38ed53892aa0a319
Yurok <predbannikov.yurii@gmail.com>
2021-07-14 13:46:55 +0300

    find the first duplicate number
    Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
    
    Example
    
    For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
    
    There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
    
    For a = [2, 2], the output should be firstDuplicate(a) = 2;
    
    For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
    
    Input/Output
    
    [execution time limit] 0.5 seconds (cpp)
    
    [input] array.integer a
    
    Guaranteed constraints:
    1 ≤ a.length ≤ 105,
    1 ≤ a[i] ≤ a.length.
    
    [output] integer
    
    The element in a that occurs in the array more than once and has the minimal index for its second occurrence. If there are no such elements, return -1.

--- solutions.cpp
+++ solutions.cpp
@@ -5,9 +5,20 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
+int firstDuplicate(std::vector<int> a) {
+	std::unordered_map<int, int> map;
+	for (auto x : a) {
+		if (map.find(x) != map.end())
+			return x;
+		else 
+			map.insert( { x, 0 });
+	}
+}
 
 int main()
 {
+
 	return 0;
 }
