02aa4f4b48c344b97e092d78ded135615da40f33
Yurok <predbannikov.yurii@gmail.com>
2021-07-14 13:01:25 +0300

    first commit

---
+++ solutions.cpp
@@ -0,0 +1,14 @@
+﻿// solutions.cpp : Defines the entry point for the application.
+//
+
+#include "solutions.h"
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
