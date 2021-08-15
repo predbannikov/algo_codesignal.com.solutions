48bfd494d5697378d13ec8bfda4962531b12db7b
Yurok <predbannikov.yurii@gmail.com>
2021-08-15 14:21:33 +0300

    nextLarger complate

--- solutions.cpp
+++ solutions.cpp
@@ -113,8 +113,8 @@ void squareBracket(std::string& s, size_t& idx) {
 	int counter = 0;
 	std::string sDigit;
 	for (; idx > 0; idx--) {
-		if (std::isdigit(s[idx-1])) {
-			sDigit.insert(0, 1, s[idx-1]);
+		if (std::isdigit(s[idx - 1])) {
+			sDigit.insert(0, 1, s[idx - 1]);
 		}
 		else
 			break;
@@ -165,10 +165,43 @@ std::string decodeString(std::string s) {
 	return s;
 }
 
+/*Note: Write a solution with O(n) complexity, since this is what you would be asked to do during a real interview.
+
+Given an array a composed of distinct elements, find the next larger element for each element of the array, i.e. the first element to the right that is greater than this element, in the order in which they appear in the array, and return the results as a new array of the same length. If an element does not have a larger element to its right, put -1 in the appropriate cell of the result array.
+
+Example
+
+For a = [6, 7, 3, 8], the output should be
+nextLarger(a) = [7, 8, 8, -1].
+
+In this array, the next larger element for 6 is 7, for 7 is 8, for 3 is 8 (7 is not a valid option since elements from a can only be compared to elements to their right), and for 8 there is no such element, so we put -1 in the last cell.*/
+
+std::vector<int> nextLarger(std::vector<int> a) {
+	for (size_t i = 0; i < a.size(); i++) {
+		size_t j = i + 1;
+		for (; j < a.size(); j++) {
+			if (a[i] < a[j]) {
+				a[i] = a[j];
+				break;
+			}
+		}
+
+		if (j == a.size())
+			a[i] = -1;
+	}
+	return a;
+}
+
 
 int main()
 {
-	std::cout << decodeString("2[20[bc]31[xy]]xd4[rt]") << std::endl;
+	std::vector<int> v1{ 10, 3, 12, 4, 2, 9, 13, 0, 8, 11, 1, 7, 5, 6 };
+	std::vector v2 = nextLarger(v1);
+	for (size_t i = 0; i < v2.size(); i++) {
+		std::cout << v2[i] << " ";
+	}
+
+	//std::cout << decodeString("2[20[bc]31[xy]]xd4[rt]") << std::endl;
 
 	//std::cout << simplifyPath("folder/subfolder/subsubfolder/.//../anotherfolder/file.txt");
 
