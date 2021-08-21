b633496e3a5530c4bf436c885687520463b3716b
Yurok <predbannikov.yurii@gmail.com>
2021-08-21 23:07:54 +0300

    pow just for fan O(log(n))

--- graphs.h
+++ graphs.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "solutions.h"
 
 /*In a multithreaded environment, it's possible that different processes will need to use the same resource. A wait-for graph represents the different processes as nodes in a directed graph, where an edge from node i to node j means that the node j is using a resource that node i needs to use (and cannot use until node j releases it).
@@ -57,11 +57,71 @@ bool hasDeadlock(std::vector<std::vector<int>> connections) {
 	return false;
 }
 
+/*Note: Try to solve this task in O(n2) time, where n is a number of vertices, since this is what you'll be asked to do during an interview.
+
+Sue is a network administrator who consults for companies that have massive Local Area Networks (LANs). The computers are connected together with network cables, and Sue has been brought in to evaluate the company’s network. The networks are huge, and she wants to ensure that no single network cable failure can disconnect the network. Any cable that fails that leaves the network in two or more disconnected pieces is called a single point of failure.
+
+She labels the different network devices from 0 to n - 1. She keeps an n × n matrix connections, where connections[i][j] = 1 if there is a network cable directly connecting devices i and j, and 0 otherwise. Write a function that takes the matrix of connections, and returns the number of cables that are a single point of failure.
+
+Example
+
+For connections = [[0, 1], [1, 0]], the output should be
+singlePointOfFailure(connections) = 1.
+A failure of the cable that connects devices 0 and 1 would leave the network disconnected.
+
+
+
+For connections = [[0, 1, 1], [1, 0, 1], [1, 1, 0]], the output should be
+singlePointOfFailure(connections) = 0.
+No failure of a single network cable would result in the network being disconnected.
+
+
+
+For connections = [[0, 1, 1, 1, 0, 0], [1, 0, 1, 0, 0, 0], [1, 1, 0, 0, 0, 0], [1, 0, 0, 0, 1, 1], [0, 0, 0, 1, 0, 0], [0, 0, 0, 1, 0, 0]], the output should be
+singlePointOfFailure(connections) = 3.
+The three cables that are single points of failure are connected with device 3:
+
+
+
+For connections = [[0, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0]], the output should be
+singlePointOfFailure(connections) = 4.
+In this topology, every cable is a single point of failure:
+
+*/
+
+int singlePointOfFailure(std::vector<std::vector<int>> connections) {
+
+	for (size_t i = 0; i < connections.size(); i++) {
+		for (size_t j = 0; j < connections[i].size(); j++) {
+			std::cout << connections[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
+int pow(int v, int n) {
+	if (n == 1)
+		return 2;
+	return pow(v, n/2) * pow(v, n/2);
+}
+
+//int pow2(int v, int n) {
+//
+//}
 
 void testGraphs() {
+	std::cout << pow(2, 7);
+	std::vector<std::vector<int>> v1 = {
+		{0, 1, 1, 1, 0, 0} ,
+		{1, 0, 1, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0},
+		{1, 0, 0, 0, 1, 1},
+		{0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 1, 0, 0},
+	};
+	std::cout << singlePointOfFailure(v1);
 
-
-	std::vector<std::vector<int>> v1 = { {1,2,3} , {2,3}, {3}, {} };
-	std::cout << std::boolalpha << hasDeadlock(v1);
-
+	//std::vector<std::vector<int>> v1 = { {1,2,3} , {2,3}, {3}, {} };
+	//std::cout << std::boolalpha << hasDeadlock(v1);
 }
